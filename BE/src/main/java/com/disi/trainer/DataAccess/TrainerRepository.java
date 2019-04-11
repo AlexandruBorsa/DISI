20f7d9a9d6122072b52f2b41dd2f9977cf93fc55
Alexandru Borsa <alexandru.borsa@p4b.ro>
2019-04-11 19:35:07 +0300

    Added UserController and UserService, created login endpoint handling post request

--- BE/src/main/java/com/disi/trainer/DataAccess/TrainerRepository.java
+++ BE/src/main/java/com/disi/trainer/DataAccess/TrainerRepository.java
@@ -2,5 +2,7 @@ package com.disi.trainer.DataAccess;
 
 import org.springframework.data.repository.CrudRepository;
 
+
 public interface TrainerRepository extends CrudRepository<Trainer, Integer> {
+    Trainer findByUsernameAndPassword(String username, String password);
 }
