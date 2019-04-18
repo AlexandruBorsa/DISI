d06725db439321166e77b37a89789c200d9e507a
ionutcente <ionut.cente@gmail.com>
2019-04-18 19:17:51 +0300

    deleted all

--- BE/src/main/java/com/disi/trainer/DataAccess/TrainerRepository.java
+++
@@ -1,6 +0,0 @@
-package com.disi.trainer.DataAccess;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface TrainerRepository extends CrudRepository<Trainer, Integer> {
-}
