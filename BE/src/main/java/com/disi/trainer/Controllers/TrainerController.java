7ed022d67ba28de8a363a976b8c510ab2b3a200e
Alexandru Borsa <alexandru.borsa@p4b.ro>
2019-04-18 18:47:28 +0300

    fixed /login and /trainers endpoints

--- BE/src/main/java/com/disi/trainer/Controllers/TrainerController.java
+++ BE/src/main/java/com/disi/trainer/Controllers/TrainerController.java
@@ -5,15 +5,14 @@ import com.disi.trainer.BusinessLogic.TrainerService;
 import com.disi.trainer.DataAccess.Trainer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
-@Controller
+@RestController
 public class TrainerController {
 
     @Autowired
@@ -25,6 +24,11 @@ public class TrainerController {
         if(!trainerService.getTrainerByUsernameAndPassword(username, password).getUsername().isEmpty())
             return "200";
         else return "404";
+    }
+
+    @RequestMapping("/trainers")
+    public List<Trainer> getAllTrainers(){
+        return trainerService.getAllTrainers();
     }
 
 }
